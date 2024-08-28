ae85e8291cb44fe13c741532ecc7d578b97ffe68
Felix Basinger <basingerfelix17@gmail.com>
2024-08-28 10:28:37 +0200

    Added changes in settings

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ CSRF_COOKIE_SECURE = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '192.168.8.177',
-    'basingerfelixblo.pythonanywhere.com',
+    'blogmkm.pythonanywhere.com',
 ]
 
 # Application definition
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'basingerfelixblo$default',
-        'USER': 'basingerfelixblo',
+        'NAME': 'blogmkm$default',
+        'USER': 'blogmkm',
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'basingerfelixblog.mysql.pythonanywhere-services.com',
+        'HOST': 'blogmkm.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
